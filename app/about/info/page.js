9355ce9858efd853af9f46c26a40e07b68dadd8f
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-03-30 07:05:53 -0700

    understood the routing in app router

---
+++ app/about/info/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const aboutInfo = () => {
+  return <div>aboutInfo</div>;
+};
+
+export default aboutInfo;
---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const aboutPage = () => {
+  return (
+    <>
+      <h1>aboutPage</h1>
+      <Link href="/about/info">about info page</Link>
+    </>
+  );
+};
+
+export default aboutPage;
