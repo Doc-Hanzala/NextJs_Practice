60813c0e89eae8a34e89953326c5ebe163a203da
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-04-01 05:31:23 -0700

    implemented loading & error functionality

---
+++ app/drinks/loading.js
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <div className="loading"></div>;
+};
+
+export default loading;
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,5 +1,24 @@
-const DrinksPage = () => {
-  return <div>DrinksPage</div>;
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(url);
+  if(!response.ok === true){
+    throw new Error("there was an error")
+  }
+  const data = response.json();
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
+  console.log(data);
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">drinks page</h1>
+    </>
+  );
 };
 
 export default DrinksPage;
