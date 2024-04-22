d1bbef571545445432e07fff2bb2edeee056b215
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-04-21 21:24:28 -0700

    worked with next img component

--- app/drinks/[singleid]/page.js
+++ app/drinks/[singleid]/page.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import drinkImg from "./drink.jpg";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
@@ -17,13 +19,20 @@ const SingleDrinkPage = async ({ params }) => {
   const title = data?.drinks[0]?.strDrink;
   const img = data?.drinks[0]?.strDrinkThumb;
 
-
   return (
     <div>
       <Link href="/drinks" className="btn btn-accent text-2xl mt-8 mb-12">
         back to drinks
       </Link>
 
+      <Image
+        src={img}
+        height={300}
+        width={300}
+        className="h-52 w-52 rounded-lg"
+        priority
+        alt={title}
+      />
       <h1 className="text-2xl">{title}</h1>
     </div>
   );
--- components/Drinks.jsx
+++ components/Drinks.jsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Drinks = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6">
       {drinks.map((drink) => {
         return (
           <li key={drink.idDrink}>
-            <Link className="text-xl font-bold  " href={`/drinks/${drink.idDrink}`}>
-              {drink.strDrink} 
+            <Link
+              className="text-xl font-bold  "
+              href={`/drinks/${drink.idDrink}`}
+            >
+              <div className="relative h-48 mb-4">
+                <Image
+                  src={drink.strDrinkThumb}
+                  fill
+                  sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw"
+                  alt={drink.strDrink}
+                  className="rounded-md object-cover"
+                />
+              </div>
+              {drink.strDrink}
             </Link>
           </li>
         );
